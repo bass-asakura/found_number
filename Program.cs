0eb97155e82473b9afd3b4f47d08c6f7e5d382ab
bass <msileha@mail.ru>
2024-11-27 16:07:40 +0300

    create full kod

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace found_number;
+
+class Program
+{
+    static void print(string qwe)
+    {
+        Console.WriteLine(qwe);
+    }
+
+    static void Main(string[] args)
+    {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            print("Число загадано, ждем ваши варианты!");
+
+        while (true)
+        {
+            int example_num = Convert.ToInt32(Console.ReadLine());
+            if (example_num > number)
+            {
+                print("Ваше число больше загаданного \n Введите еще раз");
+            }
+            else if (example_num < number)
+            {
+                print("Ваше число меньше загаданного \n Введите еще раз");
+            }
+            else
+            {
+                print("Вы угадали число!");
+                return;
+            }
+        }       
+    }
+}
